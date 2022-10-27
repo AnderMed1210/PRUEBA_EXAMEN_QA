907bd57c19cbe64e3579e41221adece395878661
AnderMed1210 <116371241+AnderMed1210@users.noreply.github.com>
2022-10-26 23:12:41 -0500

    Update For_Ejemplos.java

--- HolaMundo/src/For_Ejemplos.java
+++ HolaMundo/src/For_Ejemplos.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class For_Ejemplos {
     public static void main(String[] args) {
-        int n = 7;
+        int n = 10;
         Scanner in = new Scanner(System.in);
         System.out.println("Ingrese numero de n: ");
         n = in.nextInt();
